8de5cec0dd600b55aefb2e4c43f4244fa577aaa3
Sukensie <tom.soucek@centrum.cz>
2025-10-21 12:43:05 +0200

    Fix GitHub Pages routing and add auto-deployment

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/b2b-website">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
